11ce2b173d182d2d1d7529ef34358ba43f92542d
Reinkaos <rj.matthews23@gmail.com>
2016-12-17 13:34:33 +1300

    show view spec

--- spec/web/views/posts/show_spec.rb
+++ spec/web/views/posts/show_spec.rb
@@ -1,12 +1,32 @@
 require_relative '../../../../apps/web/views/posts/show'
 
 RSpec.describe Web::Views::Posts::Show do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { Hash[post: nil] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/posts/show.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    expect(view.foo).to eq exposures.fetch(:foo)
+  it 'exposes #post' do
+    expect(view.post).to eq exposures.fetch(:post)
+  end
+
+  describe 'when a post was not found' do
+    it 'shows a friendly message' do
+      expect(rendered).to include('Sorry, the post that you are looking for is not here.')
+    end
+  end
+
+  describe 'when a post is found' do
+    let(:post)     { Post.new(title: 'Post One', body: 'Post Body') }
+    let(:exposures) { Hash[post: post] }
+
+    it 'shows the post' do
+      expect(view.post).to eq exposures.fetch(:post)
+      expect(rendered).to include 'Post One'
+    end
+
+    it 'does not display the friendly message' do
+      expect(rendered).not_to include('Sorry, the post that you are looking for is not here.')
+    end
   end
 end
