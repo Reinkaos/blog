f835cddaa1d3a2927a9ea1a193fa38ae8adb4525
Reinkaos <rj.matthews23@gmail.com>
2016-12-27 09:15:46 +1300

    Remove commented out code

--- apps/web/controllers/posts/update.rb
+++ apps/web/controllers/posts/update.rb
@@ -1,17 +1,10 @@
 module Web::Controllers::Posts
+  # apps/webs/controllers/posts/update.rb
   class Update
     include Web::Action
 
     def call(params)
-
-      # repository = PostRepository.new
-      # post = repository.find(params[:post][:id])
-      #
-      # p post.id
-
       PostRepository.new.update(params[:post][:id], title: params[:post][:title], body: params[:post][:body])
-
-      # post = repository.update(post.id, title: 'yeh', body: 'na')
     end
   end
 end
